fdb55ef199368ec1a2bfb7dcc43fa0fea7a4bfc4
ozkar <ozkar@ozkar.org>
2016-06-26 02:38:34 -0500

    adds counting semaphore so only 20 max connections are open at once

--- checker/serverlist.go
+++ checker/serverlist.go
@@ -1,28 +1,35 @@
 package checker
 
 import (
-	"sync"
-	"net/http"
 	"errors"
+	"net/http"
+	"sync"
 )
 
 type ServerList struct {
 	Servers []Server
 	sync.WaitGroup
-	comm chan Server
+	comm    chan Server
+	maxCons int
 }
 
 func (sl *ServerList) Init() {
 	sl.comm = make(chan Server)
 	sl.Add(len(sl.Servers)) //initalize embedded waitgroup
-} 
+	sl.maxCons = 20         //20 max goroutines
+}
 
 func (serverList ServerList) Failed() []Server {
+
+	sema := make(chan struct{}, serverList.maxCons)
 	failedServerList := make([]Server, 0)
-	
+
 	for _, server := range serverList.Servers {
 		/* Fan In */
 		go func(s Server) {
+			sema <- struct{}{}        //Grab token.
+			defer func() { <-sema }() //Release token.
+
 			/* Test if site is up, send failed down the channel */
 			resp, err := http.Get(s.SchemaDomain())
 			if err != nil || resp.StatusCode != http.StatusOK {
